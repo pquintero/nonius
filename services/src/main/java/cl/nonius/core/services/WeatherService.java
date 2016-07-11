230f800a8aba31af47e526382e3e30c5f0a298b3
Patricio Quintero <pquintero@Patricios-MBP.iptv.microsoft.com>
2016-07-11 00:17:58 -0400

    Commit: Correccion de modulos web y capa de servicios

---
+++ services/src/main/java/cl/nonius/core/services/WeatherService.java
@@ -0,0 +1,15 @@
+package cl.nonius.core.services;
+
+/**
+ * Created by pquintero on 7/11/16.
+ */
+public class WeatherService {
+
+    public WeatherService() {}
+
+    public String retrieveForecast( String caracters ) throws Exception {
+
+        // Format (Print) Data
+        return caracters.toUpperCase();
+    }
+}
